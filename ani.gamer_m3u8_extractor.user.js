0e770a00957d13675d3afd2b40cee743e3e5413a
FlandreDaisuki <vbnm123c@gmail.com>
2017-07-13 15:54:36 +0800

    Formatting

--- ani.gamer_m3u8_extractor.user.js
+++ ani.gamer_m3u8_extractor.user.js
@@ -6,32 +6,19 @@
 // @author      FlandreDaisuki
 // @include     https://ani.gamer.com.tw/animeVideo.php?sn=*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @require     https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.min.js
+// @require
+// https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.min.js
 // @require     https://i2.bahamut.com.tw/js/anime.js
 // @version     0.1
 // @grant       none
 // ==/UserScript==
-const deviceId = animefun.getdeviceid();
-
-const snListNodes = $('.season .playing').parent().find('a').toArray();
-if (snListNodes.length === 0) {
-	snListNodes.push({
-		href: location.href,
-		textContent: ''
-	});
-}
-const snList = snListNodes.map(a => {
-		return {
-			sn: a.href.replace(/.*sn=(\d+)/, '$1'),
-			text: a.textContent
-		};
-	});
-
 function cFetch(url) {
 	if (url.startsWith('//')) {
 		url = 'https:' + url;
 	}
-	return fetch(url, {credentials: 'same-origin'});
+	return fetch(url, {
+		credentials: 'same-origin'
+	});
 }
 
 function genName(N) {
@@ -42,11 +29,12 @@ function genName(N) {
 }
 
 function getPlaylistSrc(sn) {
-	const url = `https://ani.gamer.com.tw/ajax/m3u8.php?sn=${sn}&device=${deviceId}`;
+	const url =
+		`https://ani.gamer.com.tw/ajax/m3u8.php?sn=${sn}&device=${deviceId}`;
 	return cFetch(url)
 		.then(res => res.json())
 		.then(json => {
-			if(json.error) {
+			if (json.error) {
 				return Promise.reject();
 			}
 			return json.src;
@@ -58,21 +46,25 @@ function parsePlayList(pl) {
 	const lines = pl.split('\n');
 	const o = {
 		version: '',
-		streams:{}
+		streams: {}
 	};
 
 	for (let i = 0; i < lines.length; i++) {
 		const line = lines[i];
 		if (line.includes('#EXT-X-')) {
-			const attr = line.replace(/#EXT-X-([^:]*):(.*)/,'$1;$2').split(';');
+			const attr = line.replace(/#EXT-X-([^:]*):(.*)/, '$1;$2')
+				.split(';');
 			if (attr[0] == 'VERSION') {
 				o.version = attr[1];
 			} else if (attr[0] == 'STREAM-INF') {
 				const resolution = attr[1].replace(/.*RESOLUTION=([^,]+).*/, '$1');
 				const res = resolution.split('x')[1] + 'p';
-				const src = lines[i+1];
+				const src = lines[i + 1];
 				i++;
-				o.streams[res] = {resolution, src};
+				o.streams[res] = {
+					resolution,
+					src
+				};
 			} else {
 				console.error('Unknown line');
 			}
@@ -81,7 +73,29 @@ function parsePlayList(pl) {
 	return o;
 }
 
-Promise.all(snList.map(a => a.sn).map(getPlaylistSrc))
+const deviceId = animefun.getdeviceid();
+
+const snListNodes = $('.season .playing')
+	.parent()
+	.find('a')
+	.toArray();
+
+if (snListNodes.length === 0) {
+	snListNodes.push({
+		href: location.href,
+		textContent: ''
+	});
+}
+
+const snList = snListNodes.map(a => {
+	return {
+		sn: a.href.replace(/.*sn=(\d+)/, '$1'),
+		text: a.textContent
+	};
+});
+
+Promise.all(snList.map(a => a.sn)
+		.map(getPlaylistSrc))
 	.then(plSrcs => {
 		return Promise.all(plSrcs.map(cFetch))
 			.then(plRess => Promise.all(plRess.map(x => x.text())));
@@ -95,27 +109,33 @@ Promise.all(snList.map(a => a.sn).map(getPlaylistSrc))
 	})
 	.catch(console.error);
 
-window.snList = snList;
-
 snList.getResolution = function(p) {
 	return this.map(ep => {
 		const src = ep.streams[p].src;
 		const magic = src.replace(/(^.)*-video.*/, '$1');
-		const m3u8 = `https://gamer-cds.cdn.hinet.net/vod/gamer/${magic}/hls-ae-2s/${src}`;
-		return {name: ep.name, m3u8};
+		const m3u8 =
+			`https://gamer-cds.cdn.hinet.net/vod/gamer/${magic}/hls-ae-2s/${src}`;
+		return {
+			name: ep.name,
+			m3u8
+		};
 	});
 };
 
-for(let res of [1080, 720, 540, 360]) {
+for (let res of [1080, 720, 540, 360]) {
 	$(`<div class="sn-res">點擊儲存 ${res}p 的 m3u8</div>`)
 		.insertAfter('.anime_name button')
 		.click(() => {
-			const blob = new Blob([JSON.stringify(snList.getResolution(`${res}p`), null, 2)], {type: "text/plain;charset=utf-8"});
+			const blob = new Blob([JSON.stringify(snList.getResolution(`${res}p`), null, 2)], {
+				type: "text/plain;charset=utf-8"
+			});
 			saveAs(blob, `m3u8_${res}p.json`);
 		});
 }
 
-$('head').append(`
+$('head')
+	.append(
+		`
 <style>
 .sn-res {
 	display: inline-block;
@@ -130,5 +150,5 @@ $('head').append(`
 	font-size: 1.3em;
 	margin: 0 5px;
 }
-</style>
-`);
+</style>`
+	);
