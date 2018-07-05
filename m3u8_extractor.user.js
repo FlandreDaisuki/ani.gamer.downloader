ab2e8f6f22deee83a04ba4021914972583888946
Chun-Hao Lien <vbnm123c@gmail.com>
2018-07-06 03:17:12 +0800

    Update m3u8_extractor.user.js

--- m3u8_extractor.user.js
+++ m3u8_extractor.user.js
@@ -6,25 +6,19 @@
 // @match       *://ani.gamer.com.tw/animeVideo.php?sn=*
 // @require     https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js
 // @require     https://cdn.rawgit.com/FlandreDaisuki/ani.gamer.downloader/master/anime.patched.js
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
 /* utility func */
 
-function $(selector) {
-	return document.querySelector(selector);
-}
-
-function $$(selector) {
-	return Array.from(document.querySelectorAll(selector));
-}
-
-function $el(name, attr = {}, cb = () => {}) {
-	const el = document.createElement(name);
-	Object.assign(el, attr);
-	cb(el);
-	return el;
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => Array.from(document.querySelectorAll(selector));
+const $el = (name, attr = {}, cb = () => {}) => {
+  const el = document.createElement(name);
+  Object.assign(el, attr);
+  cb(el);
+  return el;
 }
 
 /* DOM */
@@ -40,154 +34,146 @@ function $el(name, attr = {}, cb = () => {}) {
   </div>
 </div>
 */
+{
+  const aniMenu = $('nav>.container-player');
+  const menu = $el('div', {id: 'm3u8-dl-menu'});
+  aniMenu.parentElement.insertBefore(menu, aniMenu);
 
-const aniMenu = $('nav>.container-player');
-const menu = $el('div', {id: 'm3u8-dl-menu'});
-aniMenu.parentElement.insertBefore(menu, aniMenu);
+  const checkbox = $el('input', {
+    id: 'm3u8-dl-checkbox',
+    type: 'checkbox',
+  });
 
-const checkbox = $el('input', {
-  id: 'm3u8-dl-checkbox',
-  type: 'checkbox',
-});
-
-const label = $el('label', {
-  className: 'material-icons',
-  htmlFor: 'm3u8-dl-checkbox',
-  textContent: 'video_library',
-});
+  const label = $el('label', {
+    className: 'material-icons',
+    htmlFor: 'm3u8-dl-checkbox',
+    textContent: 'video_library',
+  });
 
 
-const grid = $el('div', {
-  id: 'm3u8-dl-options',
-}, grid => {
-  const res = ['360p', '540p', '720p', '1080p'];
-  const countType = ['episode', 'season', 'all'];
-  const countTypeCh = {
-    episode: '單集',
-    season: '整季',
-    all: '全部',
-  };
-  
-  for(const t of res) {
+  const grid = $el('div', {
+    id: 'm3u8-dl-options',
+  }, grid => {
+    const res = ['360p', '540p', '720p', '1080p'];
+    const countType = ['episode', 'season', 'all'];
+    const countTypeCh = {
+      episode: '單集',
+      season: '整季',
+      all: '全部',
+    };
+
+    for(const t of res) {
+      grid.appendChild($el('div', {
+        className: 'col-span-3',
+      }, div => {
+        const radio = $el('input', {
+          type: 'radio',
+          id: `m3u8-dl-options-${t}`,
+          name: 'm3u8-dl-options-resolution',
+          value: t,
+        });
+        const label = $el('label', {
+          textContent: t,
+          htmlFor: `m3u8-dl-options-${t}`,
+        });
+        div.appendChild(radio);
+        div.appendChild(label);
+      }));
+    }
+
+    for(const t of countType) {
+      grid.appendChild($el('div', {
+        className: 'col-span-4'
+      }, div => {
+        const radio = $el('input', {
+          type: 'radio',
+          id: `m3u8-dl-options-${t}`,
+          name: 'm3u8-dl-options-countType',
+          value: t,
+        });
+        const label = $el('label', {
+          textContent: countTypeCh[t],
+          htmlFor: `m3u8-dl-options-${t}`,
+        });
+        div.appendChild(radio);
+        div.appendChild(label);
+      }));
+    }
+
     grid.appendChild($el('div', {
-      className: 'col-span-3',
+      className: 'col-span-max'
     }, div => {
-      const radio = $el('input', {
-        type: 'radio',
-        id: `m3u8-dl-options-${t}`,
-        name: 'm3u8-dl-options-resolution',
-        value: t,
+      const button = $el('input', {
+        type: 'button',
+        id: 'm3u8-dl-options-download-btn',
+        value: '下載',
       });
-      const label = $el('label', {
-        textContent: t,
-        htmlFor: `m3u8-dl-options-${t}`,
-      });
-      div.appendChild(radio);
-      div.appendChild(label);
+      div.appendChild(button);
     }));
-  }
-  
-  for(const t of countType) {
-    grid.appendChild($el('div', {
-      className: 'col-span-4'
-    }, div => {
-      const radio = $el('input', {
-        type: 'radio',
-        id: `m3u8-dl-options-${t}`,
-        name: 'm3u8-dl-options-countType',
-        value: t,
-      });
-      const label = $el('label', {
-        textContent: countTypeCh[t],
-        htmlFor: `m3u8-dl-options-${t}`,
-      });
-      div.appendChild(radio);
-      div.appendChild(label);
-    }));
-  }
-  
-  grid.appendChild($el('div', {
-    className: 'col-span-max'
-  }, div => {
-    const button = $el('input', {
-      type: 'button',
-      id: 'm3u8-dl-options-download-btn',
-      value: '下載',
-    });
-    div.appendChild(button);
-  }));
-});
+  });
 
-menu.appendChild(checkbox);
-menu.appendChild(label);
-menu.appendChild(grid);
+  menu.appendChild(checkbox);
+  menu.appendChild(label);
+  menu.appendChild(grid);
 
-$('#m3u8-dl-options-1080p').checked = true;
-$('#m3u8-dl-options-season').checked = true;
-$('#m3u8-dl-options-download-btn').disabled = true;
-
+  $('#m3u8-dl-options-1080p').checked = true;
+  $('#m3u8-dl-options-season').checked = true;
+  $('#m3u8-dl-options-download-btn').disabled = true;
+}
 /* m3u8 parser */
 
 function ahref(href) {
-  const a = $el('a');
-  a.href = href;
-  return a.href;
+  return $el('a', {href}).href;
 }
 
-function cfetch(url) {
+function xhr(url) {
   return fetch(ahref(url), {
-		credentials: 'same-origin'
-	});
+    credentials: 'same-origin'
+  });
 }
 
-function getPlaylistSrc(sn) {
+// Playlist === MultipleResolutionM3U8
+async function getPlaylistSrc(sn) {
+  /* global animefun */
   const deviceId = animefun.getdeviceid();
-  const url = `https://ani.gamer.com.tw/ajax/m3u8.php?sn=${sn}&device=${deviceId}`;
-  return cfetch(url)
-		.then(res => res.json())
-		.then(json => {
-			if (json.error) {
-				return Promise.reject({
-          from: 'getPlaylistSrc',
-          url,
-          json,
-        });
-			}
-			return json.src;
-		})
-		.catch(console.error);
+  const url = ahref(`/ajax/m3u8.php?sn=${sn}&device=${deviceId}`);
+  const json = await xhr(url).then(resp => resp.json());
+
+  if (json.error) {
+    console.error({ from: 'getPlaylistSrc', url, json});
+    return;
+  }
+  // format:
+  //  //gamer2-cds.cdn.hinet.net/vod_gamer/_definst_/smil:gamer2(_fast)?/[\w]{40}/hls-ae-2s.smil/playlist.m3u8?token=***&expires=***&bahaData=***
+  return json.src;
 }
 
-function src2m3u8(src) {
-  const magic = src.replace(/(^.)*-video.*/, '$1');
-	return `https://gamer-cds.cdn.hinet.net/vod/gamer/${magic}/hls-ae-2s/${src}`;
-}
+function parsePlaylistText(text, playlist) {
+  const prefix = playlist.replace(/\/\/(.*hls-ae-2s.smil).*/,'$1');
+  const lines = text.split('\n');
+  const o = {};
 
-function parsePlayList(pl) {
-	const lines = pl.split('\n');
-	const o = {};
-
-	for (let i = 0; i < lines.length; i++) {
-		const line = lines[i];
-		if (line.includes('#EXT-X-')) {
-			const attr = line.replace(/#EXT-X-([^:]*):(.*)/, '$1;$2').split(';');
-			if (attr[0] == 'VERSION') {
-				// pass
-			} else if (attr[0] == 'STREAM-INF') {
-				const resolution = attr[1].replace(/.*RESOLUTION=([^,]+).*/, '$1');
-				const res = resolution.split('x')[1] + 'p';
-				const src = lines[i + 1];
-				i++;
-				o[res] = {
-					resolution,
-					m3u8: src2m3u8(src),
-				};
-			} else {
-				console.error('Unknown line');
-			}
-		}
-	}
-	return o;
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    if (line.includes('#EXT-X-')) {
+      const attr = line.replace(/#EXT-X-([^:]*):(.*)/, '$1;$2').split(';');
+      if (attr[0] == 'VERSION') {
+        // pass
+      } else if (attr[0] == 'STREAM-INF') {
+        const resolution = attr[1].replace(/.*RESOLUTION=([^,]+).*/, '$1');
+        const res = resolution.split('x')[1] + 'p';
+        const src = lines[i + 1];
+        i++;
+        o[res] = {
+          resolution,
+          m3u8: `https://${prefix}/${src}`,
+        };
+      } else {
+        console.error('Unknown line');
+      }
+    }
+  }
+  return o;
 }
 
 let episodeLinkNodes, seasonLinkNodes, allLinkNodes;
@@ -219,11 +205,11 @@ const allData = allLinkNodes.map(n => {
 /* m3u8 downloader */
 
 function isPayedAccount() {
-  return cfetch('https://ani.gamer.com.tw/animePay.php')
+  return xhr('/animePay.php')
     .then(response => {
-      console.log(response.url);
-      return response.url.includes('Payed')
-    });
+    console.log(response.url);
+    return response.url.includes('Payed')
+  });
 }
 
 function sleep(seconds) {
@@ -251,47 +237,48 @@ function sleep(seconds) {
         await sleep(5);
         playlistSrcs = await Promise.all(allTodoData.map(d => getPlaylistSrc(d.sn)));
       }
-      console.log('playlistSrcs', playlistSrcs);
     }
   } catch(e) {
     /* not logged in */
-    $$('#m3u8-dl-options input').forEach(el => el.disabled = true);
+    $$('#m3u8-dl-options input').forEach(el => { el.disabled = true; });
   }
-  
 
-  const playlistRaws = await Promise.all(playlistSrcs.map(cfetch))
-    .then(plResponses => Promise.all(plResponses.map(r => r.text())));
-  const playlists = playlistRaws.map(parsePlayList);
-  
-  const pageResponses = await Promise.all(allTodoData.map(d => cfetch(d.src)));
+  const PlaylistResponses = await Promise.all(playlistSrcs.map(xhr));
+  const PlaylistTexts = await Promise.all(PlaylistResponses.map(r => r.text()));
+  const playlists = PlaylistTexts.map((t, i) => parsePlaylistText(t, playlistSrcs[i]));
+
+  const pageResponses = await Promise.all(allTodoData.map(d => xhr(d.src)));
   const pages = await Promise.all(pageResponses.map(r => r.text()));
-  const titles = await Promise.all(pages.map(html => {
+  const titles = pages.map(html => {
     return html.replace(/[\s\S]*<title>(.*) - 巴哈姆特動畫瘋[\s\S]*/g, '$1')
       .replace('[', ' [')
       .replace('/', '／');
-  }));
-  
+  });
+
   for(const i in allTodoData) {
     const d = allTodoData[i];
     d.playlist = playlists[i];
     d.name = titles[i];
   }
-  
+
+  console.log('allTodoData', allTodoData);
+
   const downloadButton = $('#m3u8-dl-options-download-btn');
   downloadButton.disabled = false;
   downloadButton.onclick = () => {
     const resolution = $('[name="m3u8-dl-options-resolution"]:checked').value;
     const countType = $('[name="m3u8-dl-options-countType"]:checked').value;
     const final = allTodoData.filter(d => countTypeSn[countType].includes(d.sn))
-      .map(d => ({
-        name: d.name,
-        m3u8: d.playlist[resolution].m3u8,
-      }));
+    .map(d => ({
+      name: d.name,
+      m3u8: d.playlist[resolution].m3u8,
+    }));
     const blob = new Blob([JSON.stringify(final, null, 2)], {
-				type: "text/plain;charset=utf-8"
-			});
-		saveAs(blob, `playlist_${resolution}_${countType}.json`);
-    checkbox.checked = false;
+      type: "text/plain;charset=utf-8"
+    });
+    /* global saveAs */
+    saveAs(blob, `playlist_${resolution}_${countType}.json`);
+    $('#m3u8-dl-checkbox').checked = false;
   }
 })();
 
